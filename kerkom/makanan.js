cb8bce3ba7aaa3d6040f7e68420533b0866b1201
Didi <salyfaghaniya469@gmail.com>
2026-06-17 19:37:57 +0700

    teks masuk

--- kerkom/makanan.js
+++ kerkom/makanan.js
@@ -1,22 +1,25 @@
 //ini kodingan buat animasiin scrollnya
-const semuaFoodItem = document.querySelectorAll('.food-item');
+const semuaFoodItem = document.querySelectorAll(".food-item");
 const opsiScroll = {
-    root: null, 
-    threshold: 0.15, 
-    rootMargin: "0px 0px -50px 0px" 
+  root: null,
+  threshold: 0.15,
+  rootMargin: "0px 0px -50px 0px",
 };
 
-const pengintaiScroll = new IntersectionObserver(function(entries, pengintaiScroll) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('muncul'); 
-            pengintaiScroll.unobserve(entry.target); 
-        }
-    });
+const pengintaiScroll = new IntersectionObserver(function (
+  entries,
+  pengintaiScroll,
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("muncul");
+      pengintaiScroll.unobserve(entry.target);
+    }
+  });
 }, opsiScroll);
 
-semuaFoodItem.forEach(item => {
-    pengintaiScroll.observe(item);
+semuaFoodItem.forEach((item) => {
+  pengintaiScroll.observe(item);
 });
 
 //ini kodingan buat si pop up
@@ -26,19 +29,19 @@ const modalDesc = document.getElementById("modalDesc");
 
 // buat tombol kuliner diklik
 function openModal(title, description) {
-    modalTitle.innerText = title;       
-    modalDesc.innerText = description;
-    modal.classList.add("aktif");        
+  modalTitle.innerText = title;
+  modalDesc.innerHTML = description;
+  modal.classList.add("aktif");
 }
 
 // buat nutup pop up
 function closeModal() {
-    modal.classList.remove("aktif");
+  modal.classList.remove("aktif");
 }
 
 // nutup pop up kalo klik area luar pop up
-window.onclick = function(event) {
-    if (event.target == modal) {
-        closeModal();
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+};
